55a6eb27cce23bdc42edb8d9d2f274917521f5e4
codedawn <1109609196@qq.com>
2020-11-27 21:56:34 +0800

    test

---
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * @author codedawn
+ * @date 2020-11-27 21:54
+ */
+@Controller
+public class HelloController {
+    @GetMapping("")
+    public void test1(){
+
+    }
+}
